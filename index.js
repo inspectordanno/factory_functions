299eaeeb77cdc457c300dab3515a94c3ed05ca9b
Dan Spector <danno2423@gmail.com>
2018-03-17 15:31:53 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const personFactory = (name, age) => {
+  const sayHello = () => console.log('hello!');
+  return { name, age, sayHello };
+};
+
+const jeff = personFactory('jeff', 27);
+
+console.log(jeff.name); // 'jeff'
+
+jeff.sayHello(); // calls the function and logs 'hello!'
+
+const counterCreator = () => {
+  let count = 0;
+  return () => {
+    console.log(count);
+    count++;
+  };
+};
+
+const counter = counterCreator();
+
+const dog = () => {
+  const sound = 'woof'
+  return {
+    talk: () => console.log(sound)
+  }
+}
+const sniffles = dog()
+sniffles.talk()
